12aeb3e4ea94c5e29937b3f28989c30a0a73a221
Akshat Mehta <akshat2404@gmail.com>
2022-08-27 21:06:19 +0530

    Contact

--- my-portfolio/src/components/contact/Contact.jsx
+++ my-portfolio/src/components/contact/Contact.jsx
@@ -1,10 +1,19 @@
-import { useRef } from "react"
 import "./contact.css"
+import Github from "../../img/Github.png"
+import Linkedin from "../../img/Linkedin.png"
+import Mail from "../../img/Mail.png"
+import Phone from "../../img/Phone.png"
 const Contact = () => {
-    const formRef = useRef
-
-    const handleSubmit = (e)=>{
-        e.preventDefault()
+    const onButtonClick = () => {
+        fetch('Resume.pdf').then(response => {
+            response.blob().then(blob => {
+                const fileURL = window.URL.createObjectURL(blob);
+                let alink = document.createElement('a');
+                alink.href = fileURL;
+                alink.download = 'Resume.pdf';
+                alink.click();
+            })
+        })
     }
   return (
     <div className='c'>
@@ -14,29 +23,31 @@ const Contact = () => {
                 <h1 className="c-title">Feel free to contact me</h1>
                 <div className="c-info">
                     <div className="c-info-item">
-                        +91-9082609700
+                    <img src={Phone} alt="" className="c-icon"/>+91-9082609700
                     </div>
                     <div className="c-info-item">
-                        akshat2404@gmail.com
+                    <img src={Mail} alt="" className="c-icon"/>akshat2404@gmail.com
                     </div>
                     <div className="c-info-item">
-                        https://github.com/akshat240401
+                        <img src={Github} alt="" className="c-icon"/><a href="https://github.com/akshat240401">akshat240401</a>
                     </div>
                     <div className="c-info-item">
-                        https://www.linkedin.com/in/akshatmehta24/
+                        <img src={Linkedin} alt="" className="c-icon"/><a href="https://www.linkedin.com/in/akshatmehta24/">akshatmehta24</a>
                     </div>
                 </div>
             </c-left>
             <c-right>
                 <p className="c-desc">
-                    <b>Any queries?</b> Get in touch. Send and E-mail directly. Always up for work and to learn new things. Looking forward for exciting opportunities.
+                    <b>Any queries?</b> Get in touch. Always up for work and to learn new things. Looking forward for exciting opportunities.
                 </p>
                 <form>
                     <input type="text" placeholder="Name" name="user_name"/>
                     <input type="text" placeholder="Subject" name="user_subject"/>
                     <input type="text" placeholder="Email" name="user_email"/>
                     <textarea name="message" rows="5" placeholder="Message"/>
-                    <button>Submit</button>
+                    <button className="resume" onClick={onButtonClick}>
+                        Downlaod Resume
+                    </button>
                 </form>
             </c-right>
         </div>
